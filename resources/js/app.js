2bbb7c6e37c0a7360a69a015c06f1689a5a5eb3f
vilasanh <vls2016.v3@gmail.com>
2023-09-13 19:44:53 +0700

    upload first project

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import App from'./app.vue';
+
+const app = createApp(App);
+app.mount('#app-vue');
---
+++ resources/js/app.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <h1>Hello VueJS3</h1>
+
+</div>
+</template>
+
+<script>
+export default {
+name:'Minipos11App',
+data(){
+    return{
+
+    };
+},
+mounted(){
+
+},
+methods:{
+
+},
+};
+</script>
+
+<style lang = "scss" scoped >
+
+</style>
